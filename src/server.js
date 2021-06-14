cd829ac87ceba07eecd27ca7d09d8d28489eccbd
Connor Stevens <conmail1224@gmail.com>
2021-06-13 21:03:40 -0400

    Refactor logger

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 
 const PORT = process.env.PORT || 5000;
+const DEV = process.env.NODE_ENV !== 'production';
 
 const app = express();
 
 app.use(express.json());
-if (process.env.NODE_ENV === 'production') app.use(morgan('dev'));
+if (DEV) app.use(morgan('dev'));
 app.use('/', require('./routes/index.router'));
 
 app.listen(PORT, () => {
