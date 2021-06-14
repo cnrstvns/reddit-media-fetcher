46d5d5ca1a31b29762aa941c7179af9cfd54f9d4
Connor Stevens <conmail1224@gmail.com>
2021-06-13 21:00:55 -0400

    Jpeg

--- src/helpers/media.helper.js
+++ src/helpers/media.helper.js
@@ -3,6 +3,7 @@ module.exports.find = function findMedia(posts) {
     .filter((post) => post.data.url?.endsWith('jpg')
         || post.data.url?.endsWith('mp4')
         || post.data.url?.endsWith('gif')
-        || post.data.url?.endsWith('png'))
+        || post.data.url?.endsWith('png')
+        || post.data.url?.endsWith('jpeg'))
     .map((post) => post.data.url);
 };
